7306e813db6266c5494b5ef7df8812c11025517a
melamito <gaby.castro@outlook.com>
2021-06-08 23:37:59 -0400

    Home y inicio de sesion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {InicioSesionComponent} from '../app/components/inicio-sesion/inicio-sesion.component'
+import {HomeComponent} from '../app/components/home/home.component'
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'sesion',component:InicioSesionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HeatherComponent } from './components/heather/heather.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeatherComponent,
-    FooterComponent
+    FooterComponent,
+    InicioSesionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
