d9baff9f830cc4460573dc541ab641632204515f
KarinaMPalta <karina.matamala.p@mail.pucv.cl>
2021-06-13 22:01:19 -0400

    FormulariosContactoyTrabajo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,18 @@ import { ProductoComponent} from '../app/components/producto/producto.component'
 import { QuieneSomosComponent} from '../app/components/quiene-somos/quiene-somos.component'
 import { ContactoComponent} from '../app/components/contacto/contacto.component'
 import { CategoriaComponent } from '../app/components/categoria/categoria.component'
-
+import {TrabajoComponent} from '../app/components/trabajo/trabajo.component'
+import { AgradecimientoComponent } from './components/agradecimiento/agradecimiento.component';
+ 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'sesion',component:InicioSesionComponent},
   {path:'producto/:id',component:ProductoComponent},
   {path:'nosotros',component:QuieneSomosComponent},
   {path:'contacto',component:ContactoComponent},
-  {path:'categoria/:sexo/:cat',component:CategoriaComponent}
+  {path:'categoria/:sexo/:cat',component:CategoriaComponent},
+  {path:'trabajo',component:TrabajoComponent},
+  {path:'agradecimientos',component:AgradecimientoComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { HomeComponent } from './components/home/home.component';
 import { ProductoComponent } from './components/producto/producto.component';
 import { QuieneSomosComponent } from './components/quiene-somos/quiene-somos.component';
 import { CategoriaComponent } from './components/categoria/categoria.component';
+import { TrabajoComponent } from './components/trabajo/trabajo.component';
+import { AgradecimientoComponent } from './components/agradecimiento/agradecimiento.component';
+import { HorarioComponent } from './components/horario/horario.component';
 
 
 @NgModule({
@@ -23,6 +26,9 @@ import { CategoriaComponent } from './components/categoria/categoria.component';
     ProductoComponent,
     QuieneSomosComponent,
     CategoriaComponent,
+    TrabajoComponent,
+    AgradecimientoComponent,
+    HorarioComponent,
   ],
   imports: [
     BrowserModule,
